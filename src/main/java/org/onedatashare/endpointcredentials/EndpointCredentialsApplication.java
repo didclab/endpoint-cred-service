3e676956a0bd691cb5f5875377e6dd736de02c89
Jacob <jgoldverg@gmail.com>
2020-09-05 20:32:10 -0400

    added swagger docs

--- src/main/java/org/onedatashare/endpointcredentials/EndpointCredentialsApplication.java
+++ src/main/java/org/onedatashare/endpointcredentials/EndpointCredentialsApplication.java
@@ -1,5 +1,10 @@
 package org.onedatashare.endpointcredentials;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +13,22 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+        info = @Info(
+                title = "OneDataShare REST API",
+                version = "1.0",
+                description = "OpenAPI REST API documentation",
+                license = @License(name = "Apache-2.0", url = "https://github.com/didclab/onedatashare/blob/master/LICENSE"),
+                contact = @Contact(url = "http://onedatashare.org", name = "OneDataShare Team", email = "admin@onedatashare.org")
+        ),
+        servers = {
+                @Server(
+                        description = "ODS backend",
+                        url = "http://onedatashare.org"
+                )
+        }
+)
+
 public class EndpointCredentialsApplication {
     public static void main(String[] args) {
         SpringApplication.run(EndpointCredentialsApplication.class, args);
